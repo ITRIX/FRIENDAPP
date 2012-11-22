2588a367c3f3a6afefa577c11fbb6e32a4746466
Neha Upadhyay <neha1990upadhyay@gmail.com>
2012-11-23 00:36:07 +0530

    :Removed touch charts

--- touch-charts/test/issues/6092/app/Logger.js
+++
@@ -1,9 +0,0 @@
-Ext.define('SPECTRUM.Logger', {
-    statics: {
-        log: function(arg) {
-            if (window.console && window.console.log) {
-                window.console.log(arg);
-            }
-        }
-    }
-});
