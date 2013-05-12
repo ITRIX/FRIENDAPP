3d55e9b9a53c7100bbe17adcec99bf632f816933
Neha Upadhyay <neha1990upadhyay@gmail.com>
2013-05-12 19:11:19 +0530

    :Added more 2 buttons in dashboard

--- client/default/app/view/Dashboard.js
+++ client/default/app/view/Dashboard.js
@@ -1,93 +1,130 @@
 
 
 Ext.define('FRIENDAPP.view.Dashboard',{
-  xtype:'dashboard',
-  extend:'Ext.Sheet',
-  requires:[],
+    xtype:'dashboard',
+    extend:'Ext.Sheet',
+    requires:[],
   
-  config:{
-    layout:{
-      type:'vbox',
-      align:'middle',
-      pack:'center'
-    },
-    enter:'bottom',
-    exit:'bottom',
-    width:'100%',
-    height:'100%',
-    zIndex:90,
-    title:'Dashboard',
-    items:[
+    config:{
+        layout:{
+            type:'hbox'
+        },
+        enter:'bottom',
+        exit:'bottom',
+        width:'100%',
+        height:'100%',
+        zIndex:90,
+        title:'Dashboard',
+        items:[
+            {
+            xtype:'panel',
+            width:'50%',
+//            height:'100%',
+            docked:'left',
+            layout:{
+                type:'vbox',
+                align:'right',
+                pack:'center'
+            },
+            items:[
+            {      
+                xtype: 'button',
+                height: '100%',
+                width:'70%',
+                cls:'toolbarButton',
+                iconCls:'calendarCls',
+                action:'calendar'
+            }, 
+            {
+                xtype :'button',
+                height: '100%',
+                width:'70%',
+                id: "logoutButtonHit",
+                cls:'toolbarButton',
+                iconCls:'logoutCls',
+                action:'logout'
+            },
+            {
+                xtype: 'button',
+                height: '100%',
+                width:'70%',
+                cls:'toolbarButton',
+                iconCls:'graphCls',
+                action:'graph'
+            },
+            ]
+        },
+        {
+            xtype:'panel',
+            width:'50%',
+//            height:'100%',
+            docked:'right',
+            layout:{
+                type:'vbox',
+                align:'left',
+                pack:'center'
+            },
+            items:[
+            {
+                xtype: 'button',
+//                height: '100%',
+                width:'70%',
+                cls:'toolbarButton',
+                iconCls:'reportCls',
+                action:'report'
+            },
+            {
+                xtype: 'button',
+//                height: '100%',
+                width:'70%',
+                cls:'toolbarButton',
+                iconCls:'settingsCls',
+                action:'settings'
+            },
+            {
+                xtype: 'button',
+//                height: '100%',
+                width:'70%',
+                cls:'toolbarButton',
+                iconCls:'aboutusCls',
+                action:'aboutus'
+            }
+            ]
+        }
     
-//    {
-//      xtype: 'panel',
-//      width:'100%',
-//      height:'50%',
-//      layout:{
-//        type:'hbox',
-//        align:'middle',
-//        pack:'bottom'
-//      },
-//      items:[
-      {      
-        xtype: 'button',
-        height: '100%',
-        width:'50%',
-        cls:'toolbarButton',
-        iconCls:'calendarCls',
-        action:'calendar'
-      }, 
-//      {
-//        xtype:'spacer',
-//        width:'10%'
-//      },
-      {
-        xtype :'button',
-//        ui :'decline',
-        height: '100%',
-        width:'50%',
-        id: "logoutButtonHit",
-//        text :'Logout',
-//        cls: 'logoutButton',
-        cls:'toolbarButton',
-        iconCls:'logoutCls',
-        action:'logout'
-//      }]
-    },
-//    {
-//      xtype:'spacer',
-//      height:'10%'
-//    },
-//    {
-//      xtype: 'panel',
-//      width:'100%',
-//      height:'49%',
-//      layout:{
-//        type:'hbox',
-//        align:'middle',
-//        pack:'top'
-//      },
-//      items:[
-      {
-        xtype: 'button',
-       height: '100%',
-        width:'50%',
-        cls:'toolbarButton',
-        iconCls:'graphCls',
-        action:'graph'
-      },
-//      {
-//        xtype:'spacer',
-//        width:'10%'
-//      },
-      {
-        xtype: 'button',
-        height: '100%',
-        width:'50%',
-        cls:'toolbarButton',
-        iconCls:'reportCls',
-        action:'report'
-//      }]
-    }]
-  }
+//        {      
+//            xtype: 'button',
+//            height: '100%',
+//            width:'50%',
+//            cls:'toolbarButton',
+//            iconCls:'calendarCls',
+//            action:'calendar'
+//        }, 
+//        {
+//            xtype :'button',
+//            height: '100%',
+//            width:'50%',
+//            id: "logoutButtonHit",
+//            cls:'toolbarButton',
+//            iconCls:'logoutCls',
+//            action:'logout'
+//        },
+//        {
+//            xtype: 'button',
+//            height: '100%',
+//            width:'50%',
+//            cls:'toolbarButton',
+//            iconCls:'graphCls',
+//            action:'graph'
+//        },
+//        {
+//            xtype: 'button',
+//            height: '100%',
+//            width:'50%',
+//            cls:'toolbarButton',
+//            iconCls:'reportCls',
+//            action:'report'
+//        }
+        ]
+    }
 })
