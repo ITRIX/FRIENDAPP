aaf4e78c50d7cad68baa5af0a10bbb2b579f1725
Neha Upadhyay <neha1990upadhyay@gmail.com>
2013-05-13 00:11:33 +0530

    :Working on Dashboard styling

--- client/default/app/view/Dashboard.js
+++ client/default/app/view/Dashboard.js
@@ -16,10 +16,10 @@ Ext.define('FRIENDAPP.view.Dashboard',{
         zIndex:90,
         title:'Dashboard',
         items:[
-            {
+        {
             xtype:'panel',
-            width:'50%',
-//            height:'100%',
+            width:'48%',
+            //            height:'100%',
             docked:'left',
             layout:{
                 type:'vbox',
@@ -27,6 +27,10 @@ Ext.define('FRIENDAPP.view.Dashboard',{
                 pack:'center'
             },
             items:[
+            {
+                xtype:'spacer',
+                height:'10px'
+            },
             {      
                 xtype: 'button',
                 height: '100%',
@@ -35,6 +39,10 @@ Ext.define('FRIENDAPP.view.Dashboard',{
                 iconCls:'calendarCls',
                 action:'calendar'
             }, 
+            {
+                xtype:'spacer',
+                height:'10px'
+            },
             {
                 xtype :'button',
                 height: '100%',
@@ -45,6 +53,10 @@ Ext.define('FRIENDAPP.view.Dashboard',{
                 action:'logout'
             },
             {
+                xtype:'spacer',
+                height:'10px'
+            },
+            {
                 xtype: 'button',
                 height: '100%',
                 width:'70%',
@@ -52,12 +64,16 @@ Ext.define('FRIENDAPP.view.Dashboard',{
                 iconCls:'graphCls',
                 action:'graph'
             },
+            {
+                xtype:'spacer',
+                height:'10px'
+            },
             ]
         },
         {
             xtype:'panel',
-            width:'50%',
-//            height:'100%',
+            width:'48%',
+            //            height:'100%',
             docked:'right',
             layout:{
                 type:'vbox',
@@ -66,65 +82,81 @@ Ext.define('FRIENDAPP.view.Dashboard',{
             },
             items:[
             {
+                xtype:'spacer',
+                height:'10px'
+            },
+            {
                 xtype: 'button',
-//                height: '100%',
+                //                height: '100%',
                 width:'70%',
                 cls:'toolbarButton',
                 iconCls:'reportCls',
                 action:'report'
             },
             {
+                xtype:'spacer',
+                height:'10px'
+            },
+            {
                 xtype: 'button',
-//                height: '100%',
+                //                height: '100%',
                 width:'70%',
                 cls:'toolbarButton',
                 iconCls:'settingsCls',
                 action:'settings'
             },
             {
+                xtype:'spacer',
+                height:'10px'
+            },
+            {
                 xtype: 'button',
-//                height: '100%',
+                //                height: '100%',
                 width:'70%',
                 cls:'toolbarButton',
                 iconCls:'aboutusCls',
                 action:'aboutus'
-            }
+            },
+            {
+                xtype:'spacer',
+                height:'10px'
+            },
             ]
         }
     
-//        {      
-//            xtype: 'button',
-//            height: '100%',
-//            width:'50%',
-//            cls:'toolbarButton',
-//            iconCls:'calendarCls',
-//            action:'calendar'
-//        }, 
-//        {
-//            xtype :'button',
-//            height: '100%',
-//            width:'50%',
-//            id: "logoutButtonHit",
-//            cls:'toolbarButton',
-//            iconCls:'logoutCls',
-//            action:'logout'
-//        },
-//        {
-//            xtype: 'button',
-//            height: '100%',
-//            width:'50%',
-//            cls:'toolbarButton',
-//            iconCls:'graphCls',
-//            action:'graph'
-//        },
-//        {
-//            xtype: 'button',
-//            height: '100%',
-//            width:'50%',
-//            cls:'toolbarButton',
-//            iconCls:'reportCls',
-//            action:'report'
-//        }
+        //        {      
+        //            xtype: 'button',
+        //            height: '100%',
+        //            width:'50%',
+        //            cls:'toolbarButton',
+        //            iconCls:'calendarCls',
+        //            action:'calendar'
+        //        }, 
+        //        {
+        //            xtype :'button',
+        //            height: '100%',
+        //            width:'50%',
+        //            id: "logoutButtonHit",
+        //            cls:'toolbarButton',
+        //            iconCls:'logoutCls',
+        //            action:'logout'
+        //        },
+        //        {
+        //            xtype: 'button',
+        //            height: '100%',
+        //            width:'50%',
+        //            cls:'toolbarButton',
+        //            iconCls:'graphCls',
+        //            action:'graph'
+        //        },
+        //        {
+        //            xtype: 'button',
+        //            height: '100%',
+        //            width:'50%',
+        //            cls:'toolbarButton',
+        //            iconCls:'reportCls',
+        //            action:'report'
+        //        }
         ]
     }
 })
