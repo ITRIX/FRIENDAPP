8b624c873611f0948719ceb3705d31f75e17ccea
ITRIX <prakashgavade1991@gmail.com>
2013-05-13 10:22:45 -0700

    :complited passward update

--- client/default/app/controller/PasswordChangeController.js
+++ client/default/app/controller/PasswordChangeController.js
@@ -6,43 +6,79 @@
 Ext.define('FRIENDAPP.controller.PasswordChangeController', {
     extend: 'Ext.app.Controller',
     
-   config: {
+    config: {
         refs:{
-              mainFrame:'MainFrameview',
-              passwordChangeForm:'FormPasswordChange',
-              saveButton:'FormPasswordChange #btn_pass_save',
-              resetButton:'FormPasswordChange #btn_pass_reset',
-              mainPanel:'MainPanel',              
-              usernameField:'FormPasswordChange #text_username',
-              passwordField:'FormPasswordChange #text_password',
-              cpasswordField:'FormPasswordChange #text_cpassword',
-              formValidateMsg:'FormPasswordChange #formValidateMsg'
+            mainFrame:'MainFrameview',
+            passwordChangeForm:'FormPasswordChange',
+            saveButton:'FormPasswordChange #btn_pass_save',
+            resetButton:'FormPasswordChange #btn_pass_reset',
+            updateButton:'FormPasswordChange #btn_pass_update',
+            mainPanel:'MainPanel',              
+            usernameField:'FormPasswordChange #text_username',
+            passwordField:'FormPasswordChange #text_password',
+            cpasswordField:'FormPasswordChange #text_cpassword',
+            formValidateMsg:'FormPasswordChange #formValidateMsg'
         },
         
         control:{
-          resetButton:{
-              tap:'itemReset'
-          },
-          saveButton:{
-              tap:'itemSave'
-          }
+            resetButton:{
+                tap:'itemReset'
+            },
+            saveButton:{
+                tap:'itemSave'
+            },
+            updateButton:{
+                tap:'itemUpdate'
+            }
+          
         }
     },
     
     itemReset:function(){
-    this.getPasswordChangeForm().reset();
+        this.getPasswordChangeForm().reset();
     },
     
     itemSave:function(){
-          
+        if(this.dataValidate()){
+            var store=Ext.getStore('UserDataStore');
+            store.add({
+                username: this.getUsernameField().getValue(),
+                password: this.getPasswordField().getValue()
+            });
+            store.sync();
+            store.load();
+            window.location.reload();
+        }
+    },
+    
+    itemUpdate:function(){
+        if(this.dataValidate()){
+            var store=Ext.getStore('UserDataStore');             
+            if(this.getUsernameField().getValue()!==store.getById(1).get('username')){
+                this.getFormValidateMsg().setHtml('Username with Existing Username does not match!...');
+                return;
+            }else{
+                var store=Ext.getStore('UserDataStore');
+                var id=store.getById(1);
+                    id.set('username',this.getUsernameField().getValue());
+                    id.set('password', this.getPasswordField().getValue());
+                store.sync();
+                store.load();
+                this.getFormValidateMsg().setHtml('Password change Successfully!...');
+                this.itemReset();
+            }
+        }
+    },
+    
+    dataValidate:function(){
         if (!this.getUsernameField().getValue() || FRIENDAPP.util.util.isBlank(this.getUsernameField().getValue())) {
             this.getFormValidateMsg().setHtml('Please Enter Username!...');
-            return;
+            return false;
         } 
         
         if (!this.getPasswordField().getValue() || FRIENDAPP.util.util.isBlank(this.getPasswordField().getValue())) {
             this.getFormValidateMsg().setHtml('Please Enter Password!...');
-            return;
+            return false;
         }
         if (!this.getCpasswordField().getValue() || FRIENDAPP.util.util.isBlank(this.getCpasswordField().getValue())) {
             this.getFormValidateMsg().setHtml('Please Enter Confirm Password!...');
@@ -51,27 +87,10 @@ Ext.define('FRIENDAPP.controller.PasswordChangeController', {
         
         if(this.getPasswordField().getValue()!==this.getCpasswordField().getValue()){
             this.getFormValidateMsg().setHtml('Password Confirm Paasword not match!...');
-            return;
+            return false;
         }
-    
-    var store=Ext.getStore('UserDataStore');
-    store.add({username: this.getUsernameField().getValue(),
-               password: this.getPasswordField().getValue()
-             });
-    store.sync();
-    store.load();
-    debugger;
-    window.location.reload();
-    
-//    if (!this.getMainPanel()){
-//        var mainPanel=Ext.create("FRIENDAPP.view.Main");
-//        mainPanel.setActiveItem(0);
-//    }else{
-//        this.getMainPanel().setActiveItem(0);
-//    }
-//    
-    }    
-   
+        return true;
+    }
 });
 
 
